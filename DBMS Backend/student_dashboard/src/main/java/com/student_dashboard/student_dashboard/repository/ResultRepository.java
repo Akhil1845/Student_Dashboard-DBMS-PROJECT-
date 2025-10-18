0d93d1de34f88b38c587125da5c7eb4049cabbd8
Akhil1845 <itsmeakhil9999@gmail.com>
2025-10-18 09:16:01 +0530

    Refactor method names in ResultRepository

--- DBMS Backend/student_dashboard/src/main/java/com/student_dashboard/student_dashboard/repository/ResultRepository.java
+++ DBMS Backend/student_dashboard/src/main/java/com/student_dashboard/student_dashboard/repository/ResultRepository.java
@@ -2,10 +2,12 @@ package com.student_dashboard.student_dashboard.repository;
 
 import com.student_dashboard.student_dashboard.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ResultRepository extends JpaRepository<Result, Integer> {
-    List<Result> findByStudentStudentId(int studentId);
-    List<Result> findByTestTestId(int testId);
+    List<Result> findByStudent_StudentId(int studentId);
+    List<Result> findByTest_TestId(int testId);
 }
