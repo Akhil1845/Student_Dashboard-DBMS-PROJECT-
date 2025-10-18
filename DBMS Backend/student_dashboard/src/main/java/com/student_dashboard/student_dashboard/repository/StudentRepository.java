8451b0e6275286446a0178aac118922906fd90c6
Akhil1845 <itsmeakhil9999@gmail.com>
2025-10-18 09:01:18 +0530

    Remove findByEmailAndPassword method
    Removed method for finding student by email and password.

--- DBMS Backend/student_dashboard/src/main/java/com/student_dashboard/student_dashboard/repository/StudentRepository.java
+++ DBMS Backend/student_dashboard/src/main/java/com/student_dashboard/student_dashboard/repository/StudentRepository.java
@@ -11,5 +11,4 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     Optional<Student> findByEmail(String email);
 
-    Optional<Student> findByEmailAndPassword(String email, String password);
 }
